ec7fc0d53c5b7ec62703a265a43145f3829eae37
Kanaan Irwin <Kanaanci@yahoo.com>
2017-11-21 10:10:08 -0500

    Add files via upload

--- speech.js
+++ speech.js
@@ -1,16 +1,4 @@
-//   function getSpeech() {
-//      var recog = new webkitSpeechRecognition();
-// recog.lang = 'en-us';
-// recog.continuous = false;
-// recog.interimResults = false;
-// recog.maxAlternative = 5;
-// recog.onresult = function(e) {
-//    document.getElementById('saying').innerText = e.results[0][0].transcript;
-// };
-// recog.start();
-//    }
-
-	// Googles speech to text functions
+// Googles speech to text functions
 function getSpeech() {
 	var recog = new webkitSpeechRecognition();
 	recog.lang = 'en-us';
